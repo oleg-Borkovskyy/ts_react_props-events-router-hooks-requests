ce6e00dc12b2f27f908c297e6914209510bd328f
oleg <olegbnpay@gmail.com>
2021-08-26 02:16:23 +0300

    function props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Card, { CardVariant } from './components/Card';
 const App = () => {
   return (
     <div>
-      <Card width='200px' height='200px' variant={CardVariant.outline}>
+      <Card onClick={(number)=>console.log(number)} width='200px' height='200px' variant={CardVariant.outline}>
         <button>button</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 export enum CardVariant {
     outline='outline',
     primary='primary'
@@ -7,11 +7,13 @@ export enum CardVariant {
 interface CardProps {
     width?:string;
     height?:string;
-    variant:CardVariant,
+    variant:CardVariant;
+    onClick:(num:number)=> void;
 }
-const Card: FC<CardProps> = ({width,height,children,variant} ) => {
+const Card: FC<CardProps> = ({width,height,children,variant,onClick} ) => {
+    const [state,setState]=useState(0);
     return (
-        <div style={{
+        <div onClick={()=>onClick(state)} style={{
             width,
             height,
             border:variant===CardVariant.outline ? '1px solid gray':'none',
